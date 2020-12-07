d28c4c0572f0b69c1eb1b71466c8b59dc5793cbe
KOO-AL3X <72461352+KOO-AL3X@users.noreply.github.com>
2020-12-06 19:18:57 -0600

    Add files via upload

---
+++ module 9.py
@@ -0,0 +1,27 @@
+#Alex Feliciano
+#12/06/2020
+#thisaskig for you name and then the user would answer and
+#then it would say hello and the name 
+usertext = input("what is your name ")
+print("Hello", usertext)
+# this say hello world infinte
+#im going to put it in comments so i could do the other question
+#just take it off the comment to use this function 
+
+#while True:
+        #print("hello world")
+
+ #the third question if the number is divisble by 2 its true if not then its false and do it again
+# im going to put it in a comment so i could do the other question
+#def even(n):
+   # n % 2 = true
+   # if else return false
+
+   
+# hould generate a random number ‘n’
+#between 1 and 100, then print each number from 1 to n. 
+#import random
+#def numbers():
+   # x = random.randint(0,100)
+#print(1,n)
+
